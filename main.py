4716d31bb575260342299e45949154bd35b8e0c4
tknott95 <tknott95@hotmail.com>
2024-11-01 03:53:32 -0600

    MOUSE MOVES BACK_TO_POS after click event

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import pyautogui
 import time
 import random
 
-""" @NOTES - This will possibly be enhanced in another script to use openCV and AI to 
- click on change of "input img or ENUM option" rather than randomrange timer acting as an update loop
- of course you will still use an update loop but would to check diff and creatye then illusion of a player even greater.
-You then can enable randomness on click after diff is true to emulate dumbfuckery of thought. 
-This is to let you play afk mmorpg wihlst still having a joba nd being able to write code and keep life fun
-THIS IS NOT A BOT (the upgraded version could be for the fun coding) THIS IS AN AFK CLICKER TO LET ADULTS AFK PLAY BETWEEN BUILDS TO ALLEVIATE BURNOUT.
-- simply, to me, this is a proxy input via an external medium utilizing what would harwdare itself already enables. NOT A BOT. """
-
 print(" PLACE YOUR MOUSE WHERE YOU WANT TO AFK CLICK AND WAIT: \n")
 
 for i in range(2, 0, -1):
